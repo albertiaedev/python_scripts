e75c3c3e0a7aef24effb133b1353f73df786fd7d
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-06-30 11:27:43 -0400

    Source code for SQL.

---
+++ SQL/source-code.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+#Useful SQL commands
+"""
+#Create a client server
+client = bigquery.Client()
+
+#Select specific entries
+query = SELECT col FROM `folder.db.table`
+        WHERE col='row'
+"""
