eaec105d631b4818c7f07389de640050d30246ff
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-07-21 17:30:20 -0400

    Password Creator.
    This is a program that will create a secure and unhackable (or at least a very hard to hack) password for your bank accounts, private documents and mobile wallets; with the desired length that you indicate, 8-16 characters long.

--- password_creator.py
+++ password_creator.py
@@ -1,17 +1,22 @@
-#Import the modules we'll use
+#Import the modules that we'll use
 import random
 import string
 
 #Enter the length of the password
-length = int(input("Enter the desired length of the password: "))
+print("Warning: The passsword shall be 8-16 characters long.")
+length = int(input("\nEnter the desired length of the password: "))
 
 #It will create an unhackable password for your accounts
-lower_case = string.ascii_lowercase
-upper_case = string.ascii_uppercase
-number = string.digits
-symbol = string.punctuation
+if length >= 8 and length <= 16:
+  lower_case = string.ascii_lowercase
+  upper_case = string.ascii_uppercase
+  number = string.digits
+  symbol = string.punctuation
 
-create = lower_case + upper_case + number + symbol
-password = "".join(random.sample(create,length))
+  create = lower_case + upper_case + number + symbol
+  password = "".join(random.sample(create,length))
 
-print("\nCongratulations! Your password has been created:",password)
+  print("\nCongratulations! Your password has been created:",password)
+else:
+  print("\nCan't create a password.")
+#Use this password for bank accounts, private documents and mobile wallets
