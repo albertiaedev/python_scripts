b682ca6f9d9564c7b197901f0ed473b8c9aeab6d
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-08-31 11:42:56 -0400

    Image Editor.
    This is a program for you to add a black and white filter to your images.

---
+++ image_editor.py
@@ -0,0 +1,11 @@
+from PIL import Image,ImageEnhance,ImageFilter
+import os
+
+path = "./non-editedIMGs"
+pathout = "./editedIMGs"
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    edit = img.filter(ImageFilter.SHARPEN).convert("L")
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f"{pathout}/{clean_name}_edited.jpg")
